6498d138a73fdb83bf869ff45b7d93adaff610a3
bettercallazamat <bettercallazamat@gmail.com>
2020-11-20 04:41:36 +0600

    Add linter and fix offences

--- src/comments.js
+++ src/comments.js
@@ -4,25 +4,25 @@ const comments = {
     "Where is the 'DELETE COLD' button when you need it!",
     "First person to complain it's hot this summer gets punched!",
     "It's so cold outside I just saw a teenage boy with his pants pulled up.",
-    "I am sick of this cold weather",
+    'I am sick of this cold weather',
     "It's time to miss hating summer heat",
     "My favorite thing about cold weather? When it's over",
-    "The only thing II hate more than cold weather is people who say they love cold weather",
-    "Stay home. Its cold outside"
+    'The only thing II hate more than cold weather is people who say they love cold weather',
+    'Stay home. Its cold outside',
   ],
 
   normal: [
-    "Temprature outside is okay, you can cry about weather tomorrow"
+    'Temprature outside is okay, you can cry about weather tomorrow',
   ],
 
   hot: [
     "It's blooooooooody hot outside.",
-    "The heat index is somewhere in between OMG and WTF",
-    "Is it too early to start whining about the heat?",
-    "Did the hole in the ozone layer just got bombed?",
-    "You need some relationship. I mean with Air Conditioner",
-    "Lord, whatever you are baking outside.. it's done"
-  ]
+    'The heat index is somewhere in between OMG and WTF',
+    'Is it too early to start whining about the heat?',
+    'Did the hole in the ozone layer just got bombed?',
+    'You need some relationship. I mean with Air Conditioner',
+    "Lord, whatever you are baking outside.. it's done",
+  ],
 };
 
 export default comments;
--- src/helper.js
+++ src/helper.js
@@ -7,48 +7,47 @@ const kelvinToCelsius = (kelvin) => Math.round(kelvin - 273.15);
 const kelvinToFahrenheit = (kelvin) => Math.round(((kelvin - 273.15) * 9) / 5 + 32);
 
 const weatherIcon = (info) => {
-  const spanElement = document.createElement('span')
-  if (info = "Clouds") {
-    spanElement.innerHTML = '<i class="fas fa-cloud"></i>'
-  } else if (info = "Thunderstorm") {
-    spanElement.innerHTML = '<i class="fas fa-poo-storm"></i>'
-  } else if (info = "Drizzle") {
-    spanElement.innerHTML = '<i class="fas fa-cloud-rain"></i>'
-  } else if (info = "Rain") {
-    spanElement.innerHTML = '<i class="fas fa-cloud-showers-heavy"></i>'
-  } else if (info = "Snow") {
-    spanElement.innerHTML = '<i class="fas fa-snowflake"></i>'
-  } else if (info = "Clear") {
-    spanElement.innerHTML = '<i class="fas fa-meteor"></i>'
+  const spanElement = document.createElement('span');
+  if (info === 'Clouds') {
+    spanElement.innerHTML = '<i class="fas fa-cloud"></i>';
+  } else if (info === 'Thunderstorm') {
+    spanElement.innerHTML = '<i class="fas fa-poo-storm"></i>';
+  } else if (info === 'Drizzle') {
+    spanElement.innerHTML = '<i class="fas fa-cloud-rain"></i>';
+  } else if (info === 'Rain') {
+    spanElement.innerHTML = '<i class="fas fa-cloud-showers-heavy"></i>';
+  } else if (info === 'Snow') {
+    spanElement.innerHTML = '<i class="fas fa-snowflake"></i>';
+  } else if (info === 'Clear') {
+    spanElement.innerHTML = '<i class="fas fa-meteor"></i>';
   } else {
-    spanElement.innerHTML = '<i class="fas fa-bomb"></i>'
+    spanElement.innerHTML = '<i class="fas fa-bomb"></i>';
   }
 
   return spanElement;
-}
+};
 
 const generateComment = (temp, celsius) => {
   if (celsius === true && temp > 300) {
     return comments.hot[Math.floor(Math.random() * comments.hot.length)];
-  } else if (celsius === true && temp <= 300 && temp > 285) {
+  } if (celsius === true && temp <= 300 && temp > 285) {
     return comments.normal[Math.floor(Math.random() * comments.normal.length)];
-  } else {
-    return comments.cold[Math.floor(Math.random() * comments.cold.length)];
   }
-}
+  return comments.cold[Math.floor(Math.random() * comments.cold.length)];
+};
 
 const refreshBtn = (data, render) => {
-  const refresh = document.createElement('span')
+  const refresh = document.createElement('span');
   refresh.innerHTML = '<i class="fas fa-sync"></i>';
-  refresh.classList.add('refresh-btn')
+  refresh.classList.add('refresh-btn');
 
   refresh.addEventListener('click', (e) => {
     e.preventDefault();
     render(data);
-  })
+  });
 
   return refresh;
-}
+};
 
 const openModal = (modal, content) => {
   const exitModal = document.querySelector('.exit-modal');
@@ -78,5 +77,5 @@ export {
   generateComment,
   refreshBtn,
   openModal,
-  modal
+  modal,
 };
